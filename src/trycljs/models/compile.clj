d98016f578463442166932236b79e413107475f9
Tim McCormack <cortex@brainonfire.net>
2011-12-17 21:05:51 -0500

    read-string instead of read (fix last commit)

--- src/trycljs/models/compile.clj
+++ src/trycljs/models/compile.clj
@@ -3,7 +3,7 @@
 
 (defn compile-expr [cljs-expr]
   "Write text to temp file, compile, and delete the input file."
-  (comp/emits (comp/analyze {} (read cljs-expr))))
+  (comp/emits (comp/analyze {} (read-string cljs-expr))))
 
 (defn compile-request [expr]
   "Given a CLJS string, produce either a JS string or an Exception."
