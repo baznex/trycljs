feb96d76593bcaeb560e1bf9d65eeb0585b647c9
Tim McCormack <cortex@brainonfire.net>
2011-12-21 13:45:33 -0500

    Wrap input to allow multiple expressions and prettify output (not!)

--- src/trycljs/models/compile.clj
+++ src/trycljs/models/compile.clj
@@ -6,8 +6,14 @@
   (binding [comp/*cljs-ns* 'cljs.user]
       (let [env {:ns (@comp/namespaces comp/*cljs-ns*)
                  :context :statement
-                 :locals {}}]
-        (comp/emits (comp/analyze env (read-string cljs-expr))))))
+                 :locals {}}
+            ;; Wrapping in (#(last %&) ...) means that "1 2 3" will not be
+            ;; optimized to empty statement. Wrapping in pr-str will *ideally*
+            ;; give us a nice printout instead of [object Object].
+            wrapped (str "(clojure.core/pr-str (#(clojure.core/last %&) "
+                         cljs-expr
+                         " ))")]
+        (comp/emits (comp/analyze env (read-string wrapped))))))
 
 (defn compile-request [expr]
   "Given a CLJS string, produce either a JS string or an Exception."
