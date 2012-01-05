45d3b59096a5a2b6af0501270bfca1da5be94975
Tim McCormack <cortex@brainonfire.net>
2012-01-04 21:06:51 -0500

    Better wrapping, but still fails to resolve.

--- src/trycljs/models/compile.clj
+++ src/trycljs/models/compile.clj
@@ -1,19 +1,26 @@
 (ns trycljs.models.compile
   (:require [cljs.compiler :as comp]))
 
+(defn read-str-all
+  "read-string all forms, returning a possibly empty collection"
+  [s]
+  ;; The \n is to end any comment in the string
+  (rest (read-string (str "(do " s " \n)"))))
+
 (defn compile-expr [cljs-expr]
   "Write text to temp file, compile, and delete the input file."
   (binding [comp/*cljs-ns* 'cljs.user]
       (let [env {:ns (@comp/namespaces comp/*cljs-ns*)
                  :context :statement
                  :locals {}}
+            forms (binding [*ns* (create-ns comp/*cljs-ns*)]
+                    (read-str-all cljs-expr))
             ;; Wrapping in (#(last %&) ...) means that "1 2 3" will not be
             ;; optimized to empty statement. Wrapping in pr-str will *ideally*
             ;; give us a nice printout instead of [object Object].
-            wrapped (str "(clojure.core/pr-str (#(clojure.core/last %&) "
-                         cljs-expr
-                         " ))")]
-        (comp/emits (comp/analyze env (read-string wrapped))))))
+            wrapped `(do ; (ns cljs.user)
+                         (pr-str (#(last %&) ~@forms)))]
+        (comp/emits (comp/analyze env wrapped)))))
 
 (defn compile-request [expr]
   "Given a CLJS string, produce either a JS string or an Exception."
